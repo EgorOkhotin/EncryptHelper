ef514b96ae79d9e5092768ebb755a36a2bade763
Egor Okhotin <egorokhotin@gmail.com>
2019-01-29 07:18:57 +0400

    Добавьте файлы проекта.

---
+++ SampleOne/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SampleOne.Models;
+
+namespace SampleOne.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Chose()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<RedirectToActionResult> GoToChoseAsync()
+        {
+            await Task.Delay(1000);
+            return RedirectToAction("Chose");
+        }
+
+        [HttpGet]
+        public IActionResult MainLogo()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private async void Delay(int ms)
+        {
+            
+        }
+    }
+}
